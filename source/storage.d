626e466df67a03624832100aba53b4ea6f90d082
yellowsink <yellowsink@riseup.net>
2024-11-04 18:01:23 +0000

    add better storage stuff

--- source/storage.d
+++ source/storage.d
@@ -1,11 +1,15 @@
 // encrypted data storage utilities
 
-import rocksdb;
+import rocksdb : Database, WriteBatch;
 
 import types : RawAESKey;
 
-private Database open(string name)
+private:
+
+Database open(string name)
 {
+	import rocksdb : DBOptions, CompressionType;
+
 	auto opts = new DBOptions;
 	opts.createIfMissing = true;
 	opts.errorIfExists = false;
@@ -33,4 +37,110 @@ ubyte[] decrypt(const RawAESKey rkey, const ubyte[] cipher) @safe
 	auto encrypted = EncryptedData(cipher, 12, 16);
 
 	return decrypt(key, encrypted);
+}
+
+void put(DB, T, bool ENC = true)(DB db, const string key, const T value, const RawAESKey userDataKey)
+{
+	import cerealed : cerealise;
+	import std.string : representation;
+
+	auto cerealised = cerealise(value);
+
+	static if (ENC)
+		auto toPut = encrypt(userDataKey, cerealised);
+	else
+		auto toPut = cerealised;
+
+	// rocks is fast enough to do this sync
+	// cast away the immutable from the key because put() should take it const, the binding is even for `const char*`!!!
+	db.put(cast(ubyte[]) key.representation, toPut);
+}
+
+T get(DB, T, bool ENC = true)(DB db, const string key, const RawAESKey userDataKey)
+{
+	import cerealed : decerealise;
+	import std.string : representation;
+
+	auto gotten = db.get(key.representation);
+
+	static if (ENC)
+		auto cerealised = decrypt!T(userDataKey, gotten);
+	else
+		auto cerealised = gotten;
+
+	return decerealise!T(cerealised);
+}
+
+public:
+
+struct DB
+{
+	import std.typecons : Nullable;
+
+	private Database _db;
+	private Nullable!WriteBatch _wb;
+
+	this(string name)
+	{
+		_db = open(name);
+	}
+
+	~this()
+	{
+		if (!_wb.isNull) endBatch();
+
+		_db.close();
+	}
+
+	void startBatch()
+	{
+		assert(_wb.isNull, "cannot start a batch when you already have one open");
+		_wb = new WriteBatch;
+	}
+
+	void endBatch()
+	{
+		assert(!_wb.isNull, "cannot end a batch when you don't have one open");
+
+		_db.write(_wb.get);
+		_wb.nullify();
+	}
+
+	void put(T)(const string key, const T value, const RawAESKey userDataKey)
+	{
+		if (_wb.isNull)
+			.put!(Database, T, true)(_db, key, value, userDataKey);
+		else
+			.put!(WriteBatch, T, true)(_wb.get, key, value, userDataKey);
+	}
+
+	void put(T)(const string key, const T value)
+	{
+		ubyte[32] udk = void;
+		if (_wb.isNull)
+			.put!(Database, T, false)(_db, key, value, udk);
+		else
+			.put!(WriteBatch, T, false)(_wb.get, key, value, udk);
+	}
+
+	T get(T)(const string key, const RawAESKey userDataKey)
+	{
+		return .get!(Database, T, true)(_db, key, userDataKey);
+	}
+
+	T get(T)(const string key)
+	{
+		ubyte[32] udk = void;
+		return .get!(Database, T, false)(_db, key, udk);
+	}
+
+	void remove(const string key)
+	{
+		import std.string : representation;
+
+		if (_wb.isNull)
+			_db.remove(cast(ubyte[]) key.representation);
+		else
+			_wb.get.remove(cast(ubyte[]) key.representation);
+	}
 }
--- source/types.d
+++ source/types.d
@@ -1,7 +1,15 @@
 alias RawAESKey = ubyte[256/8];
 
+/// the data stored when a user is authenticated: their id and user data key
 struct AuthedUserSession
 {
 	ulong id;
-	RawAESKey key;
+	RawAESKey userDataKey;
+}
+
+/// the data given to us by the client for an access key authentication attempt: their id and access key
+struct AccessKeyAuthPair
+{
+	ulong id;
+	RawAESKey accessKey;
 }
